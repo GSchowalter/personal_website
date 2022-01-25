a1fe841443e96496c2fe87a91534e61a6e92d698
Grant Schowalter <grantschowalter@gmail.com>
2022-01-25 15:35:43 -0800

    Added main component

---
+++ src/components/MainComponent.js
@@ -0,0 +1,11 @@
+import { Container } from "reactstrap";
+
+function Main() {
+    return(
+        <>
+        <Container>
+            <p>Hello, World</p>
+        </Container>
+        </> 
+    )
+} 
