23ad9b6441ffc156e6b980dcdb1b48c07ae1e076
Grant Schowalter <grantschowalter@gmail.com>
2022-01-25 17:48:50 -0800

    Skeletons for redux

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Main from "./components/MainComponent";
 
 function App() {
   return (
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,11 +1,14 @@
 import { Container } from "reactstrap";
+import TestComponent from "./TestComponent";
 
 function Main() {
-    return(
-        <>
-        <Container>
-            <p>Hello, World</p>
-        </Container>
-        </> 
-    )
-} 
+  return (
+    <>
+      <Container>
+        <TestComponent test={"Eclipse not clutch"}></TestComponent>
+      </Container>
+    </>
+  );
+}
+
+export default Main;
